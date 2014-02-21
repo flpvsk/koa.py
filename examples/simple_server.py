6f73ce725bb7de6f4fe94348060202d0102d2042
filipovsky <filipovskii.off@gmail.com>
2014-02-21 23:53:46 +0400

    Hello server example.

--- examples/simple_server.py
+++ examples/simple_server.py
@@ -2,22 +2,28 @@ import asyncio
 from koa import Application
 
 @asyncio.coroutine
-def log_path(ctx, next):
+def log_path(ctx, nxt):
   print('Got request. {}'.format(ctx.request))
-  yield from next
-  ctx.response.status = 204
-  ctx.response.version = '1.1'
+  yield from nxt
 
 
 @asyncio.coroutine
-def log_headers(ctx, next):
+def log_headers(ctx, nxt):
   print('Headers: {}'.format(ctx.request.headers))
-  yield from next
+  yield from nxt
+
+
+@asyncio.coroutine
+def say_hello(ctx, nxt):
+  ctx.response.status = 200
+  ctx.response.write('Hello, you\'ve reached {}'.format(ctx.request.url))
+  yield from nxt
 
 
 if __name__ == '__main__':
   app = Application()
   app.use(log_path)
   app.use(log_headers)
+  app.use(say_hello)
 
   app.listen(8000)
